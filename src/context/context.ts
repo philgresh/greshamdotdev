408710349979b780091adb47c63613047a813210
Phil Gresham <phil@gresham.dev>
2022-10-27 15:14:30 -0700

    Fix TS issue

--- src/context/context.ts
+++ src/context/context.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { PortfolioContextType } from '../types';
 
+// @ts-ignore; Argument of type '{}' is not assignable to parameter of type 'PortfolioContextType'.   Type '{}' is missing the following properties from type 'PortfolioContextType': head, hero, about, projects, and 3 more.ts(2345)
 const PortfolioContext = React.createContext<PortfolioContextType>({});
 
 export const PortfolioProvider = PortfolioContext.Provider;
