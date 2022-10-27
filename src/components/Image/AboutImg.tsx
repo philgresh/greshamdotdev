f247b1f7ec32c7e569bd0c302b5b9be5ed69865a
Phil Gresham <phil@gresham.dev>
2022-10-27 14:47:06 -0700

    Refactor `Img` components

--- src/components/Image/AboutImg.tsx
+++ src/components/Image/AboutImg.tsx
@@ -1,40 +1,42 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
+import type { Images } from './types';
+
+const ABOUT_IMG_QUERY = graphql`
+  query {
+    images: allFile {
+      edges {
+        node {
+          relativePath
+          name
+          childImageSharp {
+            fixed(width: 350, quality: 100) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export type AboutImgProps = {
   filename: string;
   alt: string;
 };
 
-const AboutImg = ({ filename, alt }: AboutImgProps) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        images: allFile {
-          edges {
-            node {
-              relativePath
-              name
-              childImageSharp {
-                fixed(width: 350, quality: 100) {
-                  ...GatsbyImageSharpFixed
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={(data) => {
-      const image = data.images.edges.find((n: any) => n.node.relativePath.includes(filename));
+const AboutImg = ({ filename, alt }: AboutImgProps) => {
+  const data = useStaticQuery<Images>(ABOUT_IMG_QUERY);
 
-      if (!image) return null;
+  const image = data.images.edges.find((n: any) => n.node.relativePath === filename);
+  if (!image) return null;
 
-      const imageFixed = image.node.childImageSharp.fixed;
-      return <Img className="rounded shadow-lg" alt={alt} fixed={imageFixed} />;
-    }}
-  />
-);
+  const imageFixed = image.node.childImageSharp?.fixed;
+  const imagePublic = image.node.publicURL;
+  if (imageFixed) return <Img alt={alt} fixed={imageFixed} className="rounded shadow-lg" />;
+
+  return <img src={imagePublic} alt={alt} className="rounded shadow-lg" />;
+};
 
 export default AboutImg;
--- src/components/Image/ProjectImg.tsx
+++ src/components/Image/ProjectImg.tsx
@@ -1,50 +1,43 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
-import PropTypes from 'prop-types';
+import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
+import type { Images } from './types';
+
+const PROJECT_IMAGE_QUERY = graphql`
+  query {
+    images: allFile {
+      edges {
+        node {
+          publicURL
+          relativePath
+          name
+          childImageSharp {
+            fluid(maxWidth: 1366) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export type ProjectImgProps = {
   filename: string;
   alt: string;
-  imgClassname?: string;
 };
 
-const ProjectImg = ({ filename, alt, imgClassname }: ProjectImgProps) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query {
-          images: allFile {
-            edges {
-              node {
-                publicURL
-                relativePath
-                name
-                childImageSharp {
-                  fluid(maxWidth: 1366) {
-                    ...GatsbyImageSharpFluid
-                  }
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={(data) => {
-        const image = data.images.edges.find((n: any) => n.node.relativePath.includes(filename));
+const ProjectImg = ({ filename, alt }: ProjectImgProps) => {
+  const data = useStaticQuery<Images>(PROJECT_IMAGE_QUERY);
 
-        if (!image) return null;
+  const image = data.images.edges.find((n: any) => n.node.relativePath === filename);
+  if (!image) return null;
 
-        if (filename.includes('gif')) {
-          const imagePublic = image.node.publicURL;
-          return <img src={imagePublic} alt={alt} className={imgClassname} />;
-        }
+  const imageFluid = image.node.childImageSharp?.fluid;
+  const imagePublic = image.node.publicURL;
+  if (imageFluid) return <Img alt={alt} fluid={imageFluid} />;
 
-        const imageFluid = image.node.childImageSharp.fluid;
-        return <Img alt={alt} fluid={imageFluid} className={imgClassname} />;
-      }}
-    />
-  );
+  return <img src={imagePublic} alt={alt} />;
 };
 
 export default ProjectImg;
